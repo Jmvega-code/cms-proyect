b0290e02cf22508a07e15ea2abda807b63357a2c
Juanma Vega <juanmavega83@gmail.com>
2020-04-05 06:35:41 +0200

    update categories

--- admin/categories.php
+++ admin/categories.php
@@ -2,6 +2,7 @@
 
 
 
+
     <div id="wrapper">
 
         <!-- Navigation -->
@@ -22,27 +23,25 @@
                         <div class="col-xs-6">
 
 
-                        <?php 
+                          <?php 
 
-                        if(isset($_POST['submit'])) {
-                          $cat_title = $_POST['cat_title'];
-                         
-                          if($cat_title == "" || empty($cat_title)) {                            
-                            echo "<span style='color:red;'>This field is required</span>";
-                          } else {
+                            if(isset($_POST['submit'])) {
+                              $cat_title = $_POST['cat_title'];
+                            
+                              if($cat_title == "" || empty($cat_title)) {                            
+                                echo "<span style='color:red;'>This field is required</span>";
+                              } else {
 
-                            $query = "INSERT INTO categories(cat_title) VALUES ('{$cat_title}') ";
-                            $create_categories_query = mysqli_query($connection, $query);
-                            header('Location: categories.php');
+                                $query = "INSERT INTO categories(cat_title) VALUES ('{$cat_title}') ";
+                                $create_categories_query = mysqli_query($connection, $query);
+                                header('Location: categories.php');
 
-                            if(!$create_categories_query) {
-                              die('QUERY FAILED!' . mysqli_error($connection));
-                          }
-                        }
-                      }  
-                       ?>
-
-
+                                if(!$create_categories_query) {
+                                  die('QUERY FAILED!' . mysqli_error($connection));
+                                }
+                              }
+                            }  
+                          ?>
 
 
                           <form action="" method="POST">
@@ -53,8 +52,13 @@
                             <div class="form-group">
                               <input class="btn btn-primary" type="submit" name="submit" value="Add Category">
                             </div>
-                          </form>
-                        </div> <!--Add Category Form-->
+                          </form><!--Add Category Form-->
+
+                        
+                          <?php include "includes/update_categories.php" ?>
+
+
+                        </div> <!--Update Category Form-->
                         
                         <div class="col-xs-6">
                           <table class="table table-hover">
@@ -62,28 +66,30 @@
                               <tr>
                                 <th>Id</th>
                                 <th>Category Title</th>
-                                <th>Action</th>
+                                <th>Delete</th>
+                                <th>Update</th>
                               </tr>
                             </thead>
                             <tbody>
-                              <?php
+                                <?php
 
-                            $query = "SELECT * FROM categories";
-                            $select_categories = mysqli_query($connection, $query);
+                                  $query = "SELECT * FROM categories";
+                                  $select_categories = mysqli_query($connection, $query);
 
-                            while($row = mysqli_fetch_assoc($select_categories)){
+                                  while($row = mysqli_fetch_assoc($select_categories)){
 
-                              $cat_id = $row['cat_id'];
-                              $cat_title = $row['cat_title'];
+                                    $cat_id = $row['cat_id'];
+                                    $cat_title = $row['cat_title'];
 
-                              echo "<tr>";
-                              echo "<td>{$cat_id}</td>";
-                              echo "<td>{$cat_title}</td>";
-                              echo "<td><a style='font-size:20px'href='categories.php?delete={$cat_id}'><i class='fa fa-trash-o' ></i></a></td>";
-                              echo "</tr>";
-                            }
+                                    echo "<tr>";
+                                    echo "<td>{$cat_id}</td>";
+                                    echo "<td>{$cat_title}</td>";
+                                    echo "<td><a style='font-size:20px'href='categories.php?delete={$cat_id}'><i class='fa fa-trash-o' ></i></a>";
+                                    echo "<td><a style='font-size:20px'href='categories.php?edit={$cat_id}'><i class='fa fa-pencil' ></i></a>";
+                                    echo "</tr>";
+                                  }
 
-                            ?>
+                                ?>
 
                           <?php 
 
