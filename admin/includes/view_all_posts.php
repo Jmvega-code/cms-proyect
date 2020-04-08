8abea458e5248d4c49539d7ae690c9506cc0d339
Juanma Vega <juanmavega83@gmail.com>
2020-04-08 18:37:18 +0200

    view post add post dinamic logic

---
+++ admin/includes/view_all_posts.php
@@ -0,0 +1,29 @@
+<table class="table">
+  <thead>
+    <tr>
+      <th>Id</th>
+      <th>Author</th>
+      <th>Title</th>
+      <th>Category</th>
+      <th>Status</th>
+      <th>Image</th>
+      <th>Tags</th>
+      <th>Comments</th>
+      <th>Date</th>
+      <th>Delete</th>
+    </tr>
+  </thead>
+  <tbody>
+
+          <?php
+          // FIND ALL POSTS QUERY
+          find_all_posts();
+          ?>
+
+          <?php 
+          // DELETE CATEGORIES QUERY
+          delete_posts();
+          ?>
+
+      </tbody>
+</table>
